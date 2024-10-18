203cb81277a37c3b06a1ecbe7a561df5beaf736f
wangx <wangx@qti.qualcomm.com>
2024-10-17 20:38:04 -0700

    .

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -18,7 +18,7 @@ REQUIRED_PACKAGES = [
     'lvis',
     'scipy',
     'pandas',
-    'tf-models-official==2.5',
+    'tf-models-official',
     'tensorflow_io',
     'keras',
     'pyparsing==2.4.7',  # TODO(b/204103388)
